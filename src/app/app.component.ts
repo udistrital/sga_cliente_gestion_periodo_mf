44d8ac7918462bc3d8988f43dc6228a2df949104
Diego Celi <fernando.dfcv@gmail.com>
2024-01-30 21:54:16 -0500

    feat: udistrital/sga_documentacion#26 Se crea la aplicación angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'sga_cliente_gestionar_periodo_mf';
+}
